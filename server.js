25d5c2a57b050930ddd2df22b929d48557cfa48f
cedmondson98311 <cedmondson98311@gmail.com>
2017-03-14 21:39:22 -0700

    challenge finished

--- server.js
+++ server.js
@@ -78,6 +78,32 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   res.status(204).json(updatedItem);
 });
 
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const reqFields = ['name','id','ingredients'];
+  for(var i = 0; i < reqFields.length; i ++) {
+    const field = reqFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${reqFields[i]}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  } 
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipe \`${req.params.id}\``);
+  const updatedRecipe = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).json(updatedRecipe);
+});
+
 // when DELETE request comes in with an id in path,
 // try to delete that item from ShoppingList.
 app.delete('/shopping-list/:id', (req, res) => {
